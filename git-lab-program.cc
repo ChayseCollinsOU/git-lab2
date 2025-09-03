d7369f08781ccd13ecaf66446b995df7a165b36a
ChayseCollinsOU <cc574924@ohio.edu>
2025-09-03 17:24:49 -0400

    Add C++ Program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Chayse Collins
+ *        Date: 9/3/25
+ * Description: Print "Hello Git"
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
